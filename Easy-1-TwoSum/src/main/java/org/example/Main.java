d518a326b2992f8c8f8b74c7aebc418d60a9fde2
Janek1010 <janekdomozych99@gmail.com>
2023-12-05 20:56:10 +0100

    TwoSum better than O(n^2)!!!

--- Easy-1-TwoSum/src/main/java/org/example/Main.java
+++ Easy-1-TwoSum/src/main/java/org/example/Main.java
@@ -1,19 +1,74 @@
 package org.example;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Arrays;
+import java.util.HashMap;
+
+import static java.lang.Math.abs;
+
+/**
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * You can return the answer in any order.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: nums = [2,7,11,15], target = 9
+ * Output: [0,1]
+ * Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+ * Example 2:
+ * <p>
+ * Input: nums = [3,2,4], target = 6
+ * Output: [1,2]
+ * Example 3:
+ * <p>
+ * Input: nums = [3,3], target = 6
+ * Output: [0,1]
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * 2 <= nums.length <= 104
+ * -109 <= nums[i] <= 109
+ * -109 <= target <= 109
+ * Only one valid answer exists.
+ * <p>
+ * <p>
+ * Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+ */
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int[] numss2 = {2,7,11,15};
+        System.out.println(Arrays.toString((twoSum(numss2, 9))));
+        int[] numss = {-3,4,3,90};
+        System.out.println(Arrays.toString((twoSum(numss, 0))));
+        int[] nums = {-1,-2,-3,-4,-5};
+        System.out.println(Arrays.toString((twoSum(nums, -8))));
+        int[] nums2 = {0,4,3,0};
+        System.out.println(Arrays.toString((twoSum(nums2, 0))));
+    }
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+    public static int[] twoSum(int[] nums, int target) {
+        int[] output = {0,0};
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        //Value, Index
+        HashMap<Integer,Integer> difference= new HashMap<>();
+        int diff;
+        for (int i = 0; i < nums.length; i++) {
+            diff = target - nums[i];
+
+            if (difference.containsKey(diff)){
+
+                output[0] = difference.get(diff);
+                output[1] = i;
+                return output;
+            }
+
+                difference.put(nums[i],i);
         }
+
+        return output;
+
     }
 }
