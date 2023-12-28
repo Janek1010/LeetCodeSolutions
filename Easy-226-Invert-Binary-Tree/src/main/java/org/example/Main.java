ed326b39f45eddf1dde075b7719e54ccd19928ea
Janek1010 <janekdomozych99@gmail.com>
2023-12-28 21:39:36 +0100

    226 done recursive

--- Easy-226-Invert-Binary-Tree/src/main/java/org/example/Main.java
+++ Easy-226-Invert-Binary-Tree/src/main/java/org/example/Main.java
@@ -1,18 +1,36 @@
 package org.example;
 
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+    }
+    public static TreeNode invertTree(TreeNode root) {
+        if (root == null){
+            return null;
         }
+        TreeNode smallRoot = new TreeNode(root.val);
+        // dla kazdego poddrzewa trzeba cos
+
+        smallRoot.right = invertTree(root.left);
+        smallRoot.left =  invertTree(root.right);
+
+        return smallRoot;
     }
 }
