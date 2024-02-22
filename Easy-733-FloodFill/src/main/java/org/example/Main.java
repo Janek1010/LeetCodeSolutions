d90b48f1b7fe30b6d9042b48ade84b935be6d582
Janek1010 <janekdomozych99@gmail.com>
2024-02-22 16:54:15 +0100

    733 init

--- Easy-733-FloodFill/src/main/java/org/example/Main.java
+++ Easy-733-FloodFill/src/main/java/org/example/Main.java
@@ -1,7 +1,69 @@
 package org.example;
 
+/**
+ * An image is represented by an m x n integer grid image where image[i][j] represents the pixel value of the image.
+ *
+ * You are also given three integers sr, sc, and color. You should perform a flood fill on the image starting from the pixel image[sr][sc].
+ *
+ * To perform a flood fill, consider the starting pixel, plus any pixels connected 4-directionally to the starting pixel of the same color as the starting pixel, plus any pixels connected 4-directionally to those pixels (also with the same color), and so on. Replace the color of all of the aforementioned pixels with color.
+ *
+ * Return the modified image after performing the flood fill.
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: image = [[1,1,1],[1,1,0],[1,0,1]], sr = 1, sc = 1, color = 2
+ * Output: [[2,2,2],[2,2,0],[2,0,1]]
+ * Explanation: From the center of the image with position (sr, sc) = (1, 1) (i.e., the red pixel), all pixels connected by a path of the same color as the starting pixel (i.e., the blue pixels) are colored with the new color.
+ * Note the bottom corner is not colored 2, because it is not 4-directionally connected to the starting pixel.
+ * Example 2:
+ *
+ * Input: image = [[0,0,0],[0,0,0]], sr = 0, sc = 0, color = 0
+ * Output: [[0,0,0],[0,0,0]]
+ * Explanation: The starting pixel is already colored 0, so no changes are made to the image.
+ */
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[][] image = {{1,1,1},{1,1,0},{1,0,1}};
+        int[][] output = floodFill(image,1,1,2);
+        for (int[] ints : output) {
+            for (int anInt : ints) {
+                System.out.print(" "+anInt+", ");
+            }
+            System.out.println();
+        }
+    }
+    public static int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int colorOfField = image[sr][sc];
+        int columnLength = image[0].length;
+        int rowLength = image.length;
+
+        if (colorOfField == color){
+            return  image;
+        }
+
+        if (image[sr][sc] == colorOfField){
+            image[sr][sc] = color;
+            if (sr-1 >= 0 && image[sr-1][sc] == colorOfField){
+                floodFill(image, sr-1, sc, color);
+            }
+            if (sr+1 < rowLength&& image[sr+1][sc] == colorOfField){
+                floodFill(image, sr+1, sc, color);
+            }
+            if (sc-1 >= 0 && image[sr][sc-1] == colorOfField){
+                floodFill(image, sr, sc-1, color);
+            }
+            if (sc+1 < columnLength && image[sr][sc+1] == colorOfField){
+                floodFill(image, sr, sc+1, color);
+            }
+        } else {
+            return image;
+        }
+
+        return image;
     }
 }
