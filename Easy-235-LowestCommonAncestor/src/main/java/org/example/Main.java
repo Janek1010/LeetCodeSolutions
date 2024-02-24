1535669eee27907c33770f17aaefc1a16a5c87ea
Janek1010 <janekdomozych99@gmail.com>
2024-02-24 20:27:26 +0100

    235 working

---
+++ Easy-235-LowestCommonAncestor/src/main/java/org/example/Main.java
@@ -0,0 +1,67 @@
+package org.example;
+/**
+Given a binary search tree (BST), find the lowest common ancestor (LCA) node of two given nodes in the BST.
+
+According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+
+
+
+Example 1:
+
+
+Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+Output: 6
+Explanation: The LCA of nodes 2 and 8 is 6.
+Example 2:
+
+
+Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
+Output: 2
+Explanation: The LCA of nodes 2 and 4 is 2, since a node can be a descendant of itself according to the LCA definition.
+Example 3:
+
+Input: root = [2,1], p = 2, q = 1
+Output: 2
+
+
+Constraints:
+
+The number of nodes in the tree is in the range [2, 105].
+-109 <= Node.val <= 109
+All Node.val are unique.
+p != q
+p and q will exist in the BST.
+ **/
+public class Main {
+    public static void main(String[] args) {
+    }
+
+    public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        int valueOfNode = root.val;
+        int pVal = p.val;
+        int qVal = q.val;
+
+        boolean isPlower =false;
+        boolean isQlower = false;
+        TreeNode toReturn = root;
+
+        if (p.val < valueOfNode){
+            isPlower =  true;
+        }
+        if (q.val < valueOfNode){
+            isQlower =  true;
+        }
+        if (isPlower != isQlower || valueOfNode == pVal || valueOfNode == qVal){
+            return toReturn;
+        }
+        if (isPlower == isQlower){
+            if (isPlower){
+                toReturn = lowestCommonAncestor(root.left, p,q);
+            } else {
+                toReturn = lowestCommonAncestor(root.right, p,q);
+            }
+        }
+
+        return toReturn;
+    }
+}
